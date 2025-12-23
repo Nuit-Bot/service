ea60764dd4e988240dd5fb3dd1ade8e74f6dba4a
TotoCode <totocodefr@duck.com>
2025-12-23 11:44:49 +0100

    feat(warn): ajouter un avertissement

--- src/modules/warn.js
+++ src/modules/warn.js
@@ -2,6 +2,36 @@ import { getSupabaseClient } from "../utility/supabase.js";
 
 const supabase = getSupabaseClient();
 
-export function addWarn(userId, message) {
+/*
+Ajouter un avertissement à un utilisateur
+*/
+async function add(userId, adminId, serverId, message) {
+    // vérifier que les valeurs requises existent toutes
+    // note : message n'est PAS requis, le message sera "Pas de message" si rien est donné
+    if (!userId || !serverId || !adminId) {
+        return;
+    }
 
+    // ajouter ceci à la table supabase
+    const { error: addError } = await supabase
+        .from('warns')
+        .insert({
+            user_id: userId,
+            admin_id: adminId,
+            server_id: serverId,
+            message: message || 'Pas de message'
+        });
+
+    // vérification d'erreurs
+    if (addError) {
+        console.error(addError);
+        return addError.code;
+    }
+
+    // TODO: lorsque les logs seront rajoutés, rajouter les avertissements dedans
+
+    // succès
+    return 0;
 }
+
+export default { add };
